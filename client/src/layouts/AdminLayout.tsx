623264b3e42c6d3891cb12af33200b783057f3d4
sirdsigner <sirdsigner@gmail.com>
2026-08-18 20:02:10 +0000

    style: use favicon in admin layout

--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -42,7 +42,7 @@ const AdminLayout: React.FC = () => {
       {/* Sidebar Navigation */}
       <aside className="admin-sidebar">
         <div className="sidebar-header">
-          <img src="/logo.png" alt="BTS" className="brand-text" style={{ height: '32px' }} />
+          <img src="/favicon.png" alt="BTS" className="brand-text" style={{ height: '32px' }} />
           <span className="brand-badge">ADMIN</span>
         </div>
         <nav className="sidebar-nav">
